56954fe7dff9edecb458ea97b1fc7ddd1a763ec9
Jayman Rana <jayman.code@gmail.com>
2025-03-05 20:00:32 -0500

    create crud repo for project

--- project-management/src/main/java/com/jrp/pma/entities/Project.java
+++ project-management/src/main/java/com/jrp/pma/entities/Project.java
@@ -1,14 +1,17 @@
 package com.jrp.pma.entities;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class Project {
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long projectId;
+	
 	private String name;
 	
 	private String stage; // NOTSTARTED. COMPLETED, INPROGRESS
