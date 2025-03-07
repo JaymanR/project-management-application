d2cd43aea96d269aa6bd6bd7bc810767741d8f5d
Jayman Rana <jayman.code@gmail.com>
2025-03-07 01:15:24 -0500

    configure relational properties and seed database

--- project-management/src/main/java/com/jrp/pma/controllers/ProjectController.java
+++ project-management/src/main/java/com/jrp/pma/controllers/ProjectController.java
@@ -8,8 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.jrp.pma.dao.EmployeeRepository;
 import com.jrp.pma.dao.ProjectRepository;
+import com.jrp.pma.entities.Employee;
 import com.jrp.pma.entities.Project;
 
 @Controller
@@ -18,12 +21,15 @@ public class ProjectController {
 
 	@Autowired
 	ProjectRepository proRepo;
-	
+
+	@Autowired
+	EmployeeRepository empRepo;
+
 	@GetMapping("")
 	public String displayProjects(Model model) {
 		List<Project> projects = proRepo.findAll();
 		model.addAttribute("projects", projects);
-		
+
 		return "/projects/display-projects";
 	}
 
@@ -31,17 +37,18 @@ public class ProjectController {
 	public String displayProjectForm(Model model) {
 
 		Project aProject = new Project();
+		List<Employee> employees = empRepo.findAll();
 
 		model.addAttribute("project", aProject);
+		model.addAttribute("allEmployees", employees);
 
 		return "/projects/new-project";
 	}
 
 	@PostMapping("/save")
-	public String createProject(Project project, Model model) {
+	public String createProject(Project project, @RequestParam List<Long> employees, Model model) {
 		proRepo.save(project);
-
 		// redirecting to prevent duplicate submissions
-		return "redirect:/projects/new";
+		return "redirect:/projects";
 	}
 }
--- project-management/src/main/java/com/jrp/pma/entities/Project.java
+++ project-management/src/main/java/com/jrp/pma/entities/Project.java
@@ -1,25 +1,38 @@
 package com.jrp.pma.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class Project {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long projectId;
-	
+
 	private String name;
-	
+
 	private String stage; // NOTSTARTED. COMPLETED, INPROGRESS
-	
+
 	private String description;
-	
+
+	@ManyToMany(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH,
+			CascadeType.PERSIST }, fetch = FetchType.LAZY) // mapped by the project field on employee
+	@JoinTable(name = "project_employee", joinColumns = @JoinColumn(name = "project_id"), inverseJoinColumns = @JoinColumn(name = "employee_id"))
+	private List<Employee> employees;
+
 	public Project() {
-		
+
 	}
 
 	public Project(String name, String stage, String description) {
@@ -59,5 +72,20 @@ public class Project {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+	
+	public void addEmployee(Employee emp) {
+		if (employees == null) {
+			employees = new ArrayList<>();
+		}
+		this.employees.add(emp);
 	}
 }
